a9585db9015d2acfac30031da5b50f2258245084
Chandru T <chandrupadhur@gmail.com>
2026-07-23 20:34:32 +0530

    Refactor encryption and decryption methods

--- TranspositionCipher.java
+++ TranspositionCipher.java
@@ -3,16 +3,55 @@ class TranspositionCipher {
 
     String encrypt(String text,int key) {
 
-
         String result="";
 
 
         for(int col=0;col<key;col++) {
 
-
-            for(int i=col;i<text.length();i+=key) {
+            for(int i=col;i<text.length();i+=key)
 
                 result+=text.charAt(i);
+
+        }
+
+        return result;
+    }
+
+
+
+    String decrypt(String text,int key) {
+
+
+        char matrix[][]=new char[
+                (int)Math.ceil((double)text.length()/key)
+                ][key];
+
+
+        int k=0;
+
+
+        for(int col=0;col<key;col++) {
+
+            for(int row=0;row<matrix.length;row++) {
+
+
+                if(k<text.length())
+
+                    matrix[row][col]=text.charAt(k++);
+
+            }
+
+        }
+
+
+        String result="";
+
+
+        for(int i=0;i<matrix.length;i++) {
+
+            for(int j=0;j<key;j++) {
+
+                result+=matrix[i][j];
 
             }
 
@@ -35,9 +74,14 @@ public class Main {
         TranspositionCipher obj=new TranspositionCipher();
 
 
-        System.out.println(
-                obj.encrypt("HELLOWORLD",4)
-        );
+        String cipher=obj.encrypt("HELLOWORLD",4);
+
+
+        System.out.println(cipher);
+
+
+        System.out.println(obj.decrypt(cipher,4));
 
     }
+
 }
