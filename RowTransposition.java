bb28e967460a09a7ff5253ead0fc431563159ca2
Chandru T <chandrupadhur@gmail.com>
2026-07-23 20:34:53 +0530

    Refactor RowTransposition encryption and decryption methods

--- RowTransposition.java
+++ RowTransposition.java
@@ -1,49 +1,76 @@
 class RowTransposition {
 
 
-    String encrypt(String text,int columns) {
+    String encrypt(String text,int col) {
 
 
-        int rows=(int)Math.ceil((double)text.length()/columns);
+        int row=(int)Math.ceil((double)text.length()/col);
 
 
-        char matrix[][]=new char[rows][columns];
+        char matrix[][]=new char[row][col];
 
 
         int k=0;
 
 
-        for(int i=0;i<rows;i++) {
+        for(int i=0;i<row;i++)
 
-            for(int j=0;j<columns;j++) {
-
+            for(int j=0;j<col;j++)
 
                 if(k<text.length())
 
                     matrix[i][j]=text.charAt(k++);
 
-            }
-
-        }
 
 
-        String result="";
+        String ans="";
 
 
-        for(int j=0;j<columns;j++) {
+        for(int j=0;j<col;j++)
+
+            for(int i=0;i<row;i++)
+
+                ans+=matrix[i][j];
 
 
-            for(int i=0;i<rows;i++) {
+        return ans;
+
+    }
 
 
-                result+=matrix[i][j];
-
-            }
-
-        }
 
 
-        return result;
+    String decrypt(String text,int col) {
+
+
+        int row=(int)Math.ceil((double)text.length()/col);
+
+
+        char matrix[][]=new char[row][col];
+
+
+        int k=0;
+
+
+        for(int j=0;j<col;j++)
+
+            for(int i=0;i<row;i++)
+
+                matrix[i][j]=text.charAt(k++);
+
+
+
+        String ans="";
+
+
+        for(int i=0;i<row;i++)
+
+            for(int j=0;j<col;j++)
+
+                ans+=matrix[i][j];
+
+
+        return ans;
 
     }
 
@@ -53,14 +80,19 @@ class RowTransposition {
 
 public class Main {
 
-
     public static void main(String args[]) {
 
 
         RowTransposition obj=new RowTransposition();
 
 
-        System.out.println(obj.encrypt("HELLOWORLD",4));
+        String cipher=obj.encrypt("HELLOWORLD",4);
+
+
+        System.out.println(cipher);
+
+
+        System.out.println(obj.decrypt(cipher,4));
 
     }
 
